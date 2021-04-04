17b4500a365c251e9d3a4b2365e7d52e0c30bcc8
ricardo@sierratecnologia.com.br <ricardo@sierratecnologia.com.br>
2021-04-04 04:44:39 -0300

    RP-327 - Ajeitando e reorganiznado fluxo de telas

--- src/Services/PlanService.php
+++ src/Services/PlanService.php
@@ -120,7 +120,8 @@ class PlanService
 
             return $this->model->create($payload);
         } catch (Exception $e) {
-            dd($e->getMessage());
+            dd('PlanServiceEscritor',
+            $e->getMessage());
             throw new Exception('Could not generate new plan', 1);
         }
 
